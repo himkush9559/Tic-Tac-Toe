cf9b0c28db35f5f2cc16c15fdbd78f87b1583412
Himanshu Kushwaha <136429466+himkush9559@users.noreply.github.com>
2023-12-31 17:43:45 +0530

    change title & write some line style & js logic

---
+++ index.js
@@ -0,0 +1,10 @@
+let boxButton = document.querySelectorAll(".box-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+
+let winPattern = [
+  ["0, 1, 2"]["0, 3, 6"]["0, 4, 8"]["1,4, 7"]["2, 4, 6"]["2, 5, 8"]["3, 4, 5"][
+    "6, 7, 8"
+  ],
+];
+
